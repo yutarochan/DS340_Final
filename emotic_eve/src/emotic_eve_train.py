0627ea9fd6c5120fd1b1bb951719561bac9a67ea
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2018-04-27 22:51:03 -0400

    (add) Append EMOTIC EVE model

--- emotic_eve/src/emotic_eve_train.py
+++ emotic_eve/src/emotic_eve_train.py
@@ -11,12 +11,21 @@ from keras import backend as k
 from keras.optimizers import *
 from keras.models import Model
 from keras.applications import *
+from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 
+import params
 import util.data_csv
+from util.data_csv import EMOTICData
 
 # Original Codebase
 # https://www.kaggle.com/abnera/transfer-learning-keras-xception-cnn
+
+'''
+TODO:
+- Implement callback mechanisms for model saving and weight decay.
+- Early stopping mechanism(?)
+'''
 
 # Application Parameters
 SEED = 9892
@@ -27,12 +36,20 @@ tf.set_random_seed(seed=SEED)
 EVE_DIM = 150                       # EVE Dimensions
 IM_WIDTH, IM_HEIGHT = 255, 255      # Image Width and Height
 BATCH_SIZE = 32                     # Minibatch Size
-NB_EPOHCS = 50                      # Number of Epochs to Train Model
+NB_EPOCHS = 50                      # Number of Epochs to Train Model
 LEARN_RATE = 1e-4                   # SGD Learning Rate
 MOMENTUM = 0.9                      # SGD Momentum to Avoid Local Minimum
+BMLBLN = 126                        # alue is based on based model selected
 
+''' Load Dataset '''
+train_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_data = EMOTICData(params.DATA_DIR, params.ANNOT_DIR_TRAIN, params.EVE_MODEL_DIR)
+valid_data = EMOTICData(params.DATA_DIR, params.ANNOT_DIR_VALID, params.EVE_MODEL_DIR)
+
+''' Setup Model '''
 # Setup XceptionNet EMOTIC Model
-base_model = Xception(input_shape=(IM_WIDTH, IM_HEIGHT, 3), weights='imagenet', include_top=False)
+base_model = Xception(input_shape=params.IM_DIM, weights='imagenet', include_top=False)
 
 # Define Model Top Block
 x = base_model.output
@@ -41,10 +58,29 @@ pred = Dense(EVE_DIM)(x)
 
 # Build Model
 model = Model(base_model.input, pred)
-model.summary()
+# model.summary()
 
 # Freeze Base Model Layer - Train Top Block
 for layer in base_model.layers: layer.trainable = False
 
 # Compile Model
 model.compile(optimizer='adam', loss='mean_squared_error')
+
+''' Train Model '''
+# Train Top Layer Only
+model.fit_generator(train_data.data_gen(BATCH_SIZE), samples_per_epoch=len(train_data), nb_epoch=NB_EPOCHS)
+
+# Fine Tune Model
+# model.load_weights(top_weights_path) # Use Best Weights from Training
+
+# BMLBLN Training Activation
+for layer in model.layers[:BMLBLN]:
+    layer.trainable = False
+for layer in model.layers[BMLBLN:]:
+    layer.trainable = True
+
+# Recompile Model Again...
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Fine Tune Model
+model.fit_generator(train_data.data_gen(BATCH_SIZE), samples_per_epoch=len(train_data), nb_epoch=NB_EPOCHS)
--- emotic_eve/src/util/data_csv.py
+++ emotic_eve/src/util/data_csv.py
@@ -4,6 +4,7 @@ Author: Yuya Jeremy Ong (yjo5006@psu.edu)
 '''
 from __future__ import print_function
 import os
+import cv2
 import sys
 import time
 import warnings
@@ -12,9 +13,12 @@ import pandas as pd
 from PIL import Image
 import scipy.io as sio
 from PIL import ImageFile
+from functools import reduce
 from multiprocessing import Pool
 
-import util.eve
+# from util.eve.encode.w2v import WordEmbedding
+import params
+from util.eve.encode.w2v import WordEmbedding
 
 # FIX: Truncated Image Error
 ImageFile.LOAD_TRUNCATED_IMAGES = True
@@ -26,20 +30,19 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 # TODO: Implement thread safe method for concurrency handling and data IO pipelining to improve data load procedure to mask behind GPU.
 
 # Emotion Categories
-'''
 cat_name = ['Affection', 'Anger', 'Annoyance', 'Anticipation', 'Aversion',
             'Confidence', 'Disapproval', 'Disconnection', 'Disquietment',
             'Doubt/Confusion', 'Embarrassment', 'Engagement', 'Esteem',
             'Excitement', 'Fatigue', 'Fear', 'Happiness', 'Pain', 'Peace',
             'Pleasure', 'Sadness', 'Sensitivity', 'Suffering', 'Surprise',
             'Sympathy', 'Yearning']
-'''
 
 class EMOTICData:
-    def __init__(self, root_dir, annotations):
+    def __init__(self, root_dir, annotations, eve_model):
         # Extract Parameters
         self.ROOT_DIR = root_dir
         self.ANOT_DIR = annotations
+        self.EVE_DIR = eve_model
 
         # Load Annotation File
         start = time.time()
@@ -48,6 +51,9 @@ class EMOTICData:
 
         # Print Statement
         print('LOADED', self.ANOT_DIR, '\t[', len(self.annot),']\t', (end - start), ' sec.')
+
+        # Load EVE Model
+        self.eve = WordEmbedding(corpus=cat_name, model=self.EVE_DIR)
 
     def __len__(self):
         return len(self.annot)
@@ -58,22 +64,37 @@ class EMOTICData:
 
         # Extract Image
         # bb = self.annot.iloc[index, [1,2,3,4]].tolist()
-        image = Image.open(open(filename, 'rb')).convert('RGB')
+        image = np.array(Image.open(open(filename, 'rb')).convert('RGB'))
+        image = cv2.resize(image, (256, 256))
         # body = image.crop((int(bb[0]), int(bb[1]), int(bb[2]), int(bb[3])))
 
         # Extract and Encode Label
         category = self.annot.iloc[index, range(5, 31)].astype(int).tolist()
-        
+        cat_vecs = [self.eve.encode(cat_name[i]) for i, cat in enumerate(category) if cat == 1]
+        emo_vecs = reduce(np.add, cat_vecs)
 
-        return (image, body, category, vad)
+        return (image, emo_vecs)
+
+    def data_gen(self, batch_size):
+        X = []
+        y = []
+        for i in range(len(self)): 
+            X.append(self[i][0])
+            y.append(self[i][1])
+            
+            if i != 0 and i % batch_size == 0:
+                yield (np.array(X), np.array(y))
+                X = []
+                y = []
 
 if __name__ == '__main__':
     ROOT_DIR = '/storage/home/yjo5006/work/emotic_data/'
     ANOT_DIR = ROOT_DIR + '/emotic/train_annot.csv'
+    EVE_DIR = '/storage/home/yjo5006/DS340_Final/data/eve/emotic_w2v.pickle'
 
     # Load Training Data
     # start = time.time()
-    data = EMOTICData(ROOT_DIR+'emotic/', ANOT_DIR)
+    data = EMOTICData(ROOT_DIR+'emotic/', ANOT_DIR, EVE_DIR)
     print(data[325])
     # end = time.time()
     # print('Train - Total Time Elapsed: ' + str(end - start) + ' sec.')
